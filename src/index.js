c9a2d68de0a4d3eae6e828600d00b1370e05aa80
B-chandru <thidrkavikanchandru@gmail.com>
2024-01-21 19:16:55 +0530

    Added new name

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
-import Product from './Pages/Product';
-import Carts from './Pages/Carts';
+import Product from '../src/Pages/Product';
+import Carts from '../src/Pages/Carts';
+import { BrowserRouter, Route,Routes  } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-       <BrowserRouter>
+    <BrowserRouter>
+    <App />
       <Routes>
           <Route path="/" element={<App/>} />
           <Route path="products" element={ <Product/>} />
