a3ae2d645b3148468c16ac051a319cf1fc4faf95
B-chandru <thidrkavikanchandru@gmail.com>
2024-01-21 19:52:09 +0530

    removed a url

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
       <Routes>
           <Route path="/" element={<App/>} />
           <Route path="products" element={ <Product/>} />
